93abf121fffb161406cc1065ffde0741616d61e5
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-12 13:44:31 -0600

    PizzaPizzaLandingNo1

---
+++ src/pages/PizzaPizzaLandingPage/PizzaPízza.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from '../../components/LandingMine/HomePageComponents/Home';
+
+const PizzaPizza = () => {
+  return (
+   <div>
+    <HomePage />
+    </div>
+  );
+};
+
+export default PizzaPizza;
