a5505c223819a66f13c3b40f8752ecb015fbe574
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-13 11:28:27 -0600

    Header

--- src/components/GlobalComponents/MobileMenu.jsx
+++ src/components/GlobalComponents/MobileMenu.jsx
@@ -29,23 +29,23 @@ const MobileMenu = () => {
         <img
           src={MobileMenuIcon}
           alt="Mobile Menu"
-          className="w-20 h-auto cursor-pointer invert dark:invert-0"
+          className="w-24 h-auto cursor-pointer invert dark:invert-0"
           onClick={toggleMenu}
         />
       )}
 
       <nav
-        className={`w-full text-center text-black dark:text-white transition-all duration-500 ease-in-out overflow-hidden 
+        className={`w-full text-center text-gray-500 dark:text-white transition-all duration-500 ease-in-out overflow-hidden 
         ${menuOpen ? "max-h-[200px] opacity-100" : "max-h-0 opacity-0"} 
         ${isMobile ? "flex flex-col" : "flex flex-row max-h-none opacity-100"}`}
       >
-        <a href="/#" className="p-2 hover:text-gray-300">
+        <a href="/#" className="p-2 text-[1rem] md:text-[2rem] font-bold hover:text-gray-700 dark:hover:text-gray-400">
           Home
         </a>
-        <a href="/#" className="p-2 hover:text-gray-300">
+        <a href="/#" className="p-2 text-[1rem] md:text-[2rem] font-bold hover:text-gray-700 dark:hover:text-gray-400">
           Contact
         </a>
-        <a href="/#" className="p-2 hover:text-gray-300">
+        <a href="/#" className="p-2 text-[1rem] md:text-[2rem] font-bold hover:text-gray-700 dark:hover:text-gray-400">
           Us
         </a>
       </nav>
--- src/components/PizzaPizza/components/Header.jsx
+++ src/components/PizzaPizza/components/Header.jsx
@@ -5,14 +5,25 @@ import ImagePizzaPizza from "../img/PizzaPizza.webp";
 
 const HeaderPizzaPizza = () => {
   return (
-      <div className="grid items-center grid-cols-2 md:grid-cols-3">
-        <img src={ImagePizzaPizza} alt="ImagePizzaPizza" />
-        <DarkMode />
-        <div className="col-span-2 m-auto">
-          <MobileMenu />
-        </div>
-
+    <div className="grid items-center grid-cols-2 md:grid-cols-3 ">
+      <div className="order-1 w-auto m-2 md:order-1">
+        <img
+          src={ImagePizzaPizza}
+          alt="ImagePizzaPizza"
+          className="w-40 h-auto m-2"
+        />
       </div>
+      <div className="flex flex-row justify-end order-2 w-auto m-2 md:order-3">
+        <DarkMode />
+      </div>
+      
+      <div className="flex flex-row items-center justify-center order-3 w-full col-span-2 p-2 m-auto md:col-span-1 md:order-2">
+        <MobileMenu />
+      </div>
+
+      <div className="order-4 w-full col-span-2 border-b-4 border-gray-200 dark:border-neutral-800 md:col-span-4">
+      </div>
+    </div>
   );
 };
 
