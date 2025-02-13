9cd840c9ae1963d21416c6cb8ff89bac5b3b56d2
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-13 15:31:25 -0600

    SearchBar

--- src/components/PizzaPizza/components/Advertising.jsx
+++ src/components/PizzaPizza/components/Advertising.jsx
@@ -6,17 +6,17 @@ const Advertising = () => {
     <div className="flex flex-col justify-center w-full h-auto max-w-[80rem] bg-red-600 p-0 my-2 m-auto relative">
       <div className="relative grid grid-cols-2 p-0 m-0">
         <div className="relative z-10 flex flex-col justify-center p-0 text-center text-white">
-          <h1 className="font-bold">Discount up to 20% for</h1>
-          <h2 className="font-bold">Garlic Prawn Pizza</h2>
-          <p className="">
+          <h1 className="font-bold md:text-[2rem] lg:text[2.5rem]">Discount up to 20% for</h1>
+          <h2 className="font-bold md:text-[1.7rem] lg:text[2.2rem]">Garlic Prawn Pizza</h2>
+          <p className=" md:text-[1.2rem] lg:text[1.7rem]">
             Enjoy the delicious garlic prawn pizza at a special price!
           </p>
-          <p className="p-1 text-start">Until 20 Jan</p>
+          <p className="p-1 text-start md:text-[1rem] lg:text[1.5rem]">Until 20 Jan</p>
         </div>
 
         <div className="relative">
           <div className="relative">
-            {/* Imagen */}
+
             <img src={ImgHeader} alt="ImgHeader" className="w-full h-auto" />
 
             <div className="absolute top-0 left-0 w-[15%] h-full bg-red-600 clip-s-curve"></div>
--- src/components/PizzaPizza/components/Main.jsx
+++ src/components/PizzaPizza/components/Main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Advertising from "./Advertising";
+import SearchBar from "./SearchBar";
 
 
 const Main = () => {
   return (
     <main className="w-full h-full bg-gray-200 dark:bg-neutral-900">
         <Advertising />
+        <SearchBar />
+
     </main>
   );
 };
