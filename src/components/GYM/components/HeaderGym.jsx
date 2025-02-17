4ce9f34a9418b0806534c3ba675746efd5972a35
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-17 12:11:11 -0600

    FinishedHeader

--- src/components/GYM/components/HeaderGym.jsx
+++ src/components/GYM/components/HeaderGym.jsx
@@ -6,8 +6,7 @@ import ImageGym1 from "../img/ImageGym1.webp";
 
 const HeaderGym = () => {
   return (
-    <div className="relative flex flex-col w-full h-screen">
-
+    <div className="flex flex-col w-full h-auto">
       <img
         src={Background}
         alt="Background"
@@ -26,21 +25,27 @@ const HeaderGym = () => {
         </div>
       </header>
 
-      <div className="relative flex flex-col justify-center flex-1 text-center text-white ">
-        <h1 className="text-[1rem]">
-          EN BUSCA DE TU PRÓXIMO{" "}
-          <span className="font-bold text-[1.5rem] text-yellow-300">ENTRENAMIENTO</span>
-        </h1>
-        <a href="">
-          <button className="px-6 py-2 mt-4 font-semibold text-black transition-all duration-700 ease-out bg-yellow-300 rounded-tl-full rounded-br-full hover:bg-yellow-500">
-            COMIENZA TU VIAJE
-          </button>
-        </a>
+      <div className="grid items-center justify-center grid-cols-3 font-mono text-center text-white">
+        <div className="relative w-full col-span-3 p-4 md:col-span-2 md:top-40">
+          {" "}
+          
+          <h1 className="text-[2rem] md:text-[3rem] lg:text-[4rem] p-2">
+            EN BUSCA DE TU PRÓXIMO
+            <span className="block font-bold text-[2.5rem] md:text-[3.5rem] lg:text-[4.5rem] text-yellow-300">
+            ENTRENAMIENTO?
+            </span>
+          </h1>
+          <a href="" className="p-2">
+            <button className="w-full px-6 py-2 mt-4 font-semibold text-black transition-all duration-700 ease-out bg-yellow-300 rounded-tl-full rounded-br-full md:w-60 lg:w-96 hover:bg-yellow-500">
+              COMIENZA TU VIAJE 
+            </button>
+          </a>
+        </div>
 
         <img
           src={ImageGym1}
           alt="ImageGym1"
-          className="absolute right-0 bottom-0 w-1/3 max-w-[90rem]"
+          className="absolute right-0 bottom-0 w-1/3 max-w-[100rem] col-span-1"
         />
       </div>
     </div>
