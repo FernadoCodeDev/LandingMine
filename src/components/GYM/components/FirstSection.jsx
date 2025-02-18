34f7255eb4de8d253a8625817235b814a7d0eb8e
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-18 07:49:45 -0600

    TextFirstSection

--- src/components/GYM/components/FirstSection.jsx
+++ src/components/GYM/components/FirstSection.jsx
@@ -9,30 +9,35 @@ const FirstSection = () => {
       </div>
 
       <div className="relative p-4 text-white">
-        <h1 className="absolute leading-none inset-0 font-mono font-bold text-center opacity-10 text-neutral-500 text-[6rem] lg:text-[10rem] z-1 translate-y-6">
-          WHO WE ARE?
+        <h1 className="absolute leading-none inset-0 font-mono font-bold text-center opacity-10 text-neutral-500 text-[4rem] lg:text-[8rem] z-1 translate-y-6">
+          ¿QUIENES SOMOS?
         </h1>
 
-        <h1 className="font-mono font-bold text-start text-[2rem] lg:text-[4rem] z-2">
-          WHO WE ARE?
+        <h1 className="font-mono font-bold text-start text-[2rem]">
+          ¿QUIENES SOMOS?
         </h1>
-        <p className="text-start text-[1rem] lg:text-[1.4rem] p-1 z-2">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
-          laboriosam sunt repudiandae deserunt tenetur, odit error. Similique
-          impedit, ea minus.
+        <p className="text-start text-[1rem] lg:text-[1.4rem] p-1">
+          En Gym creemos que, independientemente de la forma y el tamaño, todos
+          estamos en un viaje hacia nuestros mejores niveles personales de salud
+          y estado físico. Un viaje que nos hace mejores como atletas, amigos y
+          personas. Nuestras instalaciones son diferentes a cualquier otro
+          gimnasio en el que hayas estado antes.
         </p>
 
-        <p className="text-start text-[1rem] lg:text-[1.4rem] p-1 z-2">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
-          laboriosam sunt repudiandae deserunt tenetur, odit error. Similique
-          impedit, ea minus.
+        <p className="text-start text-[1rem] lg:text-[1.4rem] p-1">
+          Nos enorgullecemos no solo de brindar un entrenamiento de primera
+          clase, sino que también creemos en crear un entorno motivador y
+          dinámico. Somos la comunidad dedicada a tu evolución humana, un
+          entrenamiento a la vez. ¡Ven a una <span className="text-yellow-300">clase de prueba gratuita!</span>  Pierde
+          algo de grasa corporal, gana amigos y ponte en forma para toda la
+          vida.
         </p>
       </div>
 
-    <div></div>
+      <div></div>
 
       <button className="w-full px-6 py-2 mt-4 font-semibold text-black transition-all duration-700 ease-out bg-yellow-300 rounded-tl-full rounded-br-full hover:bg-yellow-500">
-        LEARN MORE
+      MÁS INFORMACIÓN
       </button>
     </div>
   );
