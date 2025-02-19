4138b356596677e1690bb8dc0848a8ff7d152561
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-19 10:52:51 -0600

    SecondSection

--- src/components/GYM/components/SecondSection.jsx
+++ src/components/GYM/components/SecondSection.jsx
@@ -4,7 +4,6 @@ import ImageGym4 from "../img/ImageGym4.webp";
 import ImageGym5 from "../img/ImageGym5.webp";
 import ImageGym6 from "../img/ImageGym6.webp";
 
-
 const SecondSection = () => {
   return (
     <div className="grid items-center grid-cols-2 gap-4 p-4 text-center text-white md:grid-cols-4">
@@ -12,10 +11,33 @@ const SecondSection = () => {
         Ofrecemos algo para <span className="text-yellow-300">todos</span>
       </h1>
 
-      <img src={ImageGym3} alt="ImageGym2" className="w-full h-auto" />
-      <img src={ImageGym4} alt="ImageGym2" className="w-full h-auto" />
-      <img src={ImageGym5} alt="ImageGym2" className="w-full h-auto" />
-      <img src={ImageGym6} alt="ImageGym2" className="w-full h-auto" />
+      <div className="flex flex-col">
+        <img src={ImageGym3} alt="ImageGym2" className="w-full h-auto" />
+        <p className="font-mono font-bold text-white text-center text-[1rem] md:text-[2rem] p-4">
+          SOLO PARA MIEMBROS
+        </p>
+      </div>
+      <div className="flex flex-col">
+        <img src={ImageGym4} alt="ImageGym2" className="w-full h-auto" />
+
+        <p className="font-mono font-bold text-white text-center text-[1rem] md:text-[2rem] p-4">
+          ENTRENAMIENTO PERSONAL
+        </p>
+      </div>
+
+      <div className="flex flex-col">
+        <img src={ImageGym5} alt="ImageGym2" className="w-full h-auto" />
+        <p className="font-mono font-bold text-white text-center text-[1rem] md:text-[2rem] p-4">
+          CLASES DE CROSSFIT
+        </p>
+      </div>
+
+      <div className="flex flex-col">
+        <img src={ImageGym6} alt="ImageGym2" className="w-full h-auto" />
+        <p className="font-mono font-bold text-white text-center text-[rem] md:text-[2rem] p-4">
+          ENTRENAMIENTO DE FUERZA
+        </p>
+      </div>
     </div>
   );
 };
