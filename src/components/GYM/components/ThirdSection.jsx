56000eff0f25c111577968e63b96e59a82d43ff6
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-19 12:01:00 -0600

    ImgTime

--- src/components/GYM/components/ThirdSection.jsx
+++ src/components/GYM/components/ThirdSection.jsx
@@ -3,8 +3,17 @@ import React from "react";
 const ThirdSection = () => {
   return (
     <div>
-      <div className="flex flex-col justify-center text-center bg-yellow-300 w-full h-20 ">
-        <h1>VIVA LA EXPERIENCIA DE FITNESS DEFINITIVA</h1>
+      <div className="flex flex-col justify-center w-full h-20 text-center bg-yellow-300 ">
+        <h1 className="font-mono font-bold text-[1rem] lg:text-[2rem]">VIVE LA EXPERIENCIA DE FITNESS DEFINITIVA</h1>
+      </div>
+
+      <div className="grid justify-center grid-cols-3">
+
+        <div className="flex flex-col justify-center p-4 bg-slate-100">
+            <h1 className="font-bold text-center">FLEXI</h1>
+
+        </div>
+
       </div>
     </div>
   );
