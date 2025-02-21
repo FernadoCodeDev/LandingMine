0823c709406a0b05d55b776e5c4dc3c2a6ede4fb
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-21 15:27:43 -0600

    FirstSection

--- src/components/MarketingAgency/components/FirtstSection.jsx
+++ src/components/MarketingAgency/components/FirtstSection.jsx
@@ -24,60 +24,60 @@ const FirstSection = () => {
             distinctio iusto? Vero optio dignissimos velit odit voluptatum
             repellendus esse ipsum.
           </p>
-          <button className="flex fl mt-4 px-6 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-full transition-all">
+          <button className="flex fl mt-4 px-6 py-2 bg-slate-50 hover:bg-slate-300 text-black rounded-full transition-all">
             Botón de acción
           </button>
         </div>
       </div>
 
-      <div className="relative ">
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 m-auto w-full max-w-[90rem] bg-neutral-700 backdrop-blur-md p-6 rounded-lg text-white">
+      <div className="relative pb-4">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 m-auto w-full h-full max-w-[90rem] bg-neutral-700 backdrop-blur-md p-6 rounded-lg text-white">
           <div className="bg-neutral-600">
             <div className="flex flex-row justify-start w-20 h-20 bg-neutral-800 p-2">
               <SVG1 />
             </div>
-            <h2 className="text-xl font-semibold">Título</h2>
-            <p className="text-sm">Descripción breve del elemento .</p>
+            <h2 className="text-xl font-semibold p-2">SEO</h2>
+            <p className="text-sm p-2">Through organic SEO, paid search and GPS-enabled search, reach customers on an all-new level.</p>
           </div>
 
           <div className="bg-neutral-600">
             <div className="flex flex-row justify-start w-20 h-20 bg-neutral-800 p-2">
               <SVG2 />
             </div>
-            <h2 className="text-xl font-semibold">Título</h2>
-            <p className="text-sm">Descripción breve del elemento .</p>
+            <h2 className="text-xl font-semibold p-2">Social Media</h2>
+            <p className="text-sm p-2">Increase your online following and deliver messages and promotions that wow your audience.</p>
           </div>
 
           <div className="bg-neutral-600">
             <div className="flex flex-row justify-start w-20 h-20 bg-neutral-800 p-2">
               <SVG3 />
             </div>
-            <h2 className="text-xl font-semibold">Título</h2>
-            <p className="text-sm">Descripción breve del elemento .</p>
+            <h2 className="text-xl font-semibold p-2">Email</h2>
+            <p className="text-sm p-2">Reach all your customers at once with creatively designed, strategic email messaging.</p>
           </div>
 
           <div className="bg-neutral-600">
             <div className="flex flex-row justify-start w-20 h-20 bg-neutral-800 p-2">
               <SVG4 />
             </div>
-            <h2 className="text-xl font-semibold">Título</h2>
-            <p className="text-sm">Descripción breve del elemento .</p>
+            <h2 className="text-xl font-semibold p-2">Paid Search</h2>
+            <p className="text-sm p-2">Customers are searching for a business like yours! Help them find you with paid search ads.</p>
           </div>
 
           <div className="bg-neutral-600">
             <div className="flex flex-row justify-start w-20 h-20 bg-neutral-800 p-2">
               <SVG5 />
             </div>
-            <h2 className="text-xl font-semibold">Título</h2>
-            <p className="text-sm">Descripción breve del elemento .</p>
+            <h2 className="text-xl font-semibold p-2">Web Design</h2>
+            <p className="text-sm p-2">Discover our beautiful, lead-generating websites that convert visitors into customers.</p>
           </div>
 
           <div className="bg-neutral-600">
             <div className="flex flex-row justify-start w-20 h-20 bg-neutral-800 p-2">
               <SVG6 />
             </div>
-            <h2 className="text-xl font-semibold">Título</h2>
-            <p className="text-sm">Descripción breve del elemento .</p>
+            <h2 className="text-xl font-semibold p-2">Content</h2>
+            <p className="text-sm p-2">Embrace your one-of-a-kind voice with the help of custom-crafted, search-friendly content.</p>
           </div>
         </div>
       </div>
