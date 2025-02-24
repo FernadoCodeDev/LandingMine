31a3c29641cfb0df70631ef44a0fdf650da02853
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-24 13:00:21 -0600

    FirtstSection

--- src/components/PigBank/components/FirtstSection.jsx
+++ src/components/PigBank/components/FirtstSection.jsx
@@ -5,63 +5,61 @@ import SVG2 from "./svgcomponets/svg2";
 import SVG3 from "./svgcomponets/svg3";
 import SVG4 from "./svgcomponets/svg4";
 
-
-
 const FirstSection = () => {
   return (
     <div className="relative w-full min-h-screen p-4 bg-gray-100 dark:bg-neutral-800">
       <div className="grid grid-cols-1 md:grid-cols-2 ">
-        <div className="">
+        <div>
           <h1 className="text-4xl font-bold text-black dark:text-white">
-            The Strategic choice
+            La elección estratégica
           </h1>
           <p className="text-black text-l dark:text-white text-start">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
-            similique delectus expedita dolorum perspiciatis saepe ratione
+            Nuestra misión es brindar transparencia a las finanzas y mostrarle
+            todo de antemano.
           </p>
         </div>
       </div>
 
-      <div className="grid grid-cols-1 gap-4 mt-5 md:grid-cols-3">
-        <div>
+      <div className="grid grid-cols-1 gap-4 mt-5 md:grid-cols-3 md:grid-rows-2 md:items-center">
+        <div className="flex flex-col items-start gap-4 text-black dark:text-white">
           <div className="w-20 h-20 p-2 bg-red-400 rounded-full">
             <SVG1 />
           </div>
-          <h1>Lorem</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est </p>
-        </div>
+          <h1 className="text-3xl font-bold md:text-5xl">Autenticación</h1>
+          <p>Tu cuenta está protegida mediante un proceso de autenticación.</p>
 
-        <div>
           <div className="w-20 h-20 p-2 bg-red-400 rounded-full">
             <SVG2 />
           </div>
-          <h1>Lorem</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est </p>
+          <h1 className="text-3xl font-bold md:text-5xl">
+            Equipos de seguridad
+          </h1>
+          <p>
+            Nuestro equipo de seguridad trabajan para mantener
+            su dinero seguro
+          </p>
         </div>
 
-        <div>
-          <div className="w-20 h-20 p-2 bg-red-400 rounded-full">
-            <SVG3 />
-          </div>
-          <h1>Lorem</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est </p>
-        </div>
-
-        <div>
-          <div className="w-20 h-20 p-2 bg-red-400 rounded-full">
-            <SVG4 />
-          </div>
-          <h1>Lorem</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est </p>
-        </div>
-
-
-        <div className="md:order-2">
+        <div className="flex items-start justify-start w-full h-full md:mt-36 md:col-span-1 md:row-span-2">
           <img
             src={PingBankCard}
             alt="PingBankCard"
-            className="w-full h-auto md:rotate-[70deg]"
+            className="w-full h-auto object-contain md:rotate-[70deg]"
           />
+        </div>
+
+        <div className="flex flex-col items-start gap-4 text-black dark:text-white">
+          <div className="w-20 h-20 p-2 bg-red-400 rounded-full">
+            <SVG3 />
+          </div>
+          <h1 className="text-3xl font-bold md:text-5xl">Centro de Cuentas</h1>
+          <p>Guarda todas tus cuentas en un solo lugar.</p>
+
+          <div className="w-20 h-20 p-2 bg-red-400 rounded-full">
+            <SVG4 />
+          </div>
+          <h1 className="text-3xl font-bold md:text-5xl">Fondos de seguridad</h1>
+          <p>Guarde su dinero en instituciones financieras establecidas</p>
         </div>
       </div>
     </div>
