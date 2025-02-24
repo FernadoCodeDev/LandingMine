fbc8553e9447fc11726dd02dbf239a5c3eb45c9e
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-24 08:49:53 -0600

    Trapeze Header

--- src/components/PigBank/components/Header.jsx
+++ src/components/PigBank/components/Header.jsx
@@ -7,17 +7,29 @@ const HeaderPB = () => {
   return (
     <div className="relative w-full min-h-screen">
       <div className="grid items-center justify-between grid-cols-2 p-4 md:grid-cols-3">
-        <div className="order-1">
-          <img src={PigBankLogo} alt="PigBankLogo" className="w-full h-auto" />
+        <div className="order-1 m-auto">
+          <img
+            src={PigBankLogo}
+            alt="PigBankLogo"
+            className="w-full h-auto md:max-w-80"
+          />
         </div>
 
-        <div className="order-3 col-span-2 md:col-span-1 md:order-2 ">
-          <MobileMenu />
+        <div className="order-3 col-span-2 m-auto md:col-span-1 md:order-2 ">
+          <MobileMenu
+            textColor="text-black dark:text-white"
+            hoverColor="hover:text-red-500 dark:hover:text-red-500 "
+            bgColor="bg-transparent"
+            invertColor="invert dark:invert-0"
+          />
         </div>
-        <div className="order-2">
+        <div className="order-2 m-auto">
           <DarkMode />
         </div>
       </div>
+
+      <div className="absolute w-1/2 max-w-[10rem] md:max-w-[100rem] rounded-tr-3xl left-0 bottom-0 bg-red-600 h-40 clip-trapezoid-right"></div>
+      <div className="absolute w-1/2 max-w-[10rem] md:max-w-[100rem] rounded-tl-3xl right-0 bottom-0 bg-red-400 h-40 clip-trapezoid-left"></div>
     </div>
   );
 };
