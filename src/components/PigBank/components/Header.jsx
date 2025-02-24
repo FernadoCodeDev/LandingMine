b94e002eaf090f6af6ab716d004ead86d3c0fb42
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-24 08:02:55 -0600

    HeaderPB

--- src/components/PigBank/components/Header.jsx
+++ src/components/PigBank/components/Header.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import PigBankLogo from "../img/PigBankLogoNo.1.webp";
+import MobileMenu from "../../GlobalComponents/MobileMenu";
+import DarkMode from "../../GlobalComponents/DarkMode";
 
 const HeaderPB = () => {
   return (
     <div className="relative w-full min-h-screen">
-        <h1>PigBank</h1>      
+      <div className="grid items-center justify-between grid-cols-2 p-4 md:grid-cols-3">
+        <div className="order-1">
+          <img src={PigBankLogo} alt="PigBankLogo" className="w-full h-auto" />
+        </div>
+
+        <div className="order-3 col-span-2 md:col-span-1 md:order-2 ">
+          <MobileMenu />
+        </div>
+        <div className="order-2">
+          <DarkMode />
+        </div>
+      </div>
     </div>
   );
 };
