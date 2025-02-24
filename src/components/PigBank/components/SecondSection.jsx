828028c5abd00d1930403e8b038bced1fbfcd06e
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-24 16:15:46 -0600

    ''

--- src/components/PigBank/components/SecondSection.jsx
+++ src/components/PigBank/components/SecondSection.jsx
@@ -11,15 +11,21 @@ import SVGManage from "./svgcomponets/SVG-Manage";
 const SecondSection = () => {
   return (
     <div className="relative w-full min-h-screen bg-red-500">
-      <div className="grid justify-between grid-cols-1 md:grid-cols-2">
+      <div className="grid justify-between grid-cols-1 p-2 text-white md:grid-cols-2">
         <div>
-          <div>Integration</div>
-          <h1>Lorem</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-            dolores...
+          <div className="w-1/3 p-2 text-center bg-red-400 rounded-lg">
+            <h1>INTEGRACIÓN</h1>
+          </div>
+          <h1 className="text-3xl font-bold">Compra en cualquier lugar</h1>
+          <p className="p-2 text-start max-w-[40rem]">
+            Ahora es mucho más fácil pagar a alguien. Todo lo que necesitas es
+            una dirección de correo electrónico para enviar pagos comerciales
+            transfronterizos en más de 25 divisas a casi cualquier persona, en
+            cualquier lugar.
           </p>
-          <button className="bg-white">Lorem</button>
+          <button className="p-3 mt-2 font-bold text-black transition-all duration-700 ease-out bg-white rounded-full hover:bg-gray-200">
+          Explorar más
+          </button>
         </div>
 
         {/* Masonry efect*/}
@@ -28,7 +34,7 @@ const SecondSection = () => {
           className="flex items-center justify-center gap-2 p-4 text-center"
           columnClassName="flex flex-col"
         >
-          <div >
+          <div>
             <div className="p-4 mb-2 bg-red-400 rounded-3xl">
               <SVGemailing />
             </div>
