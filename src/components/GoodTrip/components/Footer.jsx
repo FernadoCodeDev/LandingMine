0cbc85b5d8e89d992b0530755e87d624185133e6
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-27 17:29:46 -0600

    FooterGoodTrip

--- src/components/GoodTrip/components/Footer.jsx
+++ src/components/GoodTrip/components/Footer.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full p-6 text-white bg-neutral-800">
+    <footer className="w-full p-6 text-black bg-orange-300">
       <div className="max-w-6xl mx-auto">
         <p className="mb-4 text-lg leading-relaxed">
-          Marketing Agency expresa su más sincero agradecimiento al talentoso diseñador
-          diseñadora{" "}
+          Good Trip expresa su más sincero agradecimiento al talentoso diseñador
+   {" "}
           <a
-            href="https://dribbble.com/hello1usman"
-            className="ml-1 font-bold text-white transition duration-300 ease-in-out hover:text-yellow-400"
+            href="https://dribbble.com/dhiradanuarta"
+            className="ml-1 font-bold text-black transition duration-300 ease-in-out hover:text-white"
           >
-            Usman A
+            Dhira Danuarta
           </a>{" "}
           cuya obra sirvió de inspiración para la creación de la landing page{" "}
-          <span className="font-bold">Marketing Agency</span>. Utilicé su diseño como
+          <span className="font-bold">Good Trip</span>. Utilicé su diseño como
           referencia para desarrollar esta interpretación propia, respetando
           siempre su estilo.
         </p>
 
         <p className="mb-4 text-lg leading-relaxed">
-          Cabe aclarar que <span className="font-bold">Marketing Agency</span> no es
+          Cabe aclarar que <span className="font-bold">Good Trip</span> no es
           una agencia real y fue creado únicamente con fines de
           desarrollo y diseño. Además, yo diseñé el logotipo de{" "}
-          <span className="font-bold">Marketing Agency</span> y utilicé imágenes de
+          <span className="font-bold">Good Trip</span> y utilicé imágenes de
           uso libre en su construcción.
         </p>
 
@@ -33,13 +33,13 @@ const Footer = () => {
         </p>
       </div>
 
-      <div className="mt-8 border-t-2 border-white opacity-50"></div>
+      <div className="mt-8 border-t-2 border-black opacity-50"></div>
 
       <p className="mt-6 text-sm text-center">
-        © 2025 Marketing Agency.{" "}
+        © 2025 Good Trip.{" "}
         <a
           href="/"
-          className="ml-1 font-bold text-white transition duration-300 ease-in-out hover:text-yellow-400"
+          className="ml-1 font-bold text-black transition duration-300 ease-in-out hover:text-white"
         >
           Regresar a LandingMine
         </a>
