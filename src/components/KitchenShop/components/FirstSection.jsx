daab82ff52e1fb3247ff63a00ce8baba9f0572a7
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-28 15:25:31 -0600

    FirstSection

--- src/components/KitchenShop/components/FirstSection.jsx
+++ src/components/KitchenShop/components/FirstSection.jsx
@@ -2,8 +2,30 @@ import React from "react";
 
 const FirstSection = () => {
   return (
-    <div className="relative w-full p-4 text-black dark:text-white min-h-auto ">
+    <div className="relative w-full p-4 bg-stone-300 text-stone-700 dark:text-slate-200 bg-bg-stone-300 dark:bg-neutral-800 min-h-auto ">
+      <h1 className="font-mono font-bold text-3xl md:text-5xl text-start mb-6 mx-auto max-w-[70rem]">
+        DESCUBRE LA COLECCIÓN DURADERA
+      </h1>
 
+      <div className="flex flex-col justify-between p-2 md:flex-row dark:text-white md:col-span-3">
+        <button className="p-2 font-bold border-b-2 cursor-pointer text-stone-800 dark:text-slate-100 border-stone-800 dark:border-slate-100">
+          LOS MÁS VENDIDOS
+        </button>
+        <button className="p-2 font-bold transition duration-700 ease-out border-b-2 cursor-pointer dark:border-neutral-700 hover:border-stone-800 dark:hover:hover:border-slate-100">
+          EQUIPOS
+        </button>
+        <button className="p-2 font-bold transition duration-700 ease-out border-b-2 cursor-pointer dark:border-neutral-700 hover:border-stone-800 dark:hover:hover:border-slate-100">
+          utensilios
+        </button>
+        <button className="p-2 font-bold transition duration-700 ease-out border-b-2 cursor-pointer dark:border-neutral-700 hover:border-stone-800 dark:hover:hover:border-slate-100">
+          HERRAMIENTAS
+        </button>
+      </div>
+      
+      <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
+        <div className=""></div>
+ 
+      </div>
     </div>
   );
 };
