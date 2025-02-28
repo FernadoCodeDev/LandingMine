2fc58cf0c98bd00ceec338682ce8d1220e0353a1
Fernando <fernando.e.mejia.e@gmail.com>
2025-02-28 15:28:47 -0600

    ArrowSVG

---
+++ src/components/KitchenShop/components/SVGComponets/ArrowSVG.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+const ArrowSVG = (props) => {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M14 2H5.50003L4.00003 3.5L6.83581 6.33579L0.585815 12.5858L3.41424 15.4142L9.66424 9.16421L12.5 12L14 10.5L14 2Z"
+          fill="#ffffff"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default ArrowSVG;
