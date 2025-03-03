efc07b80ce6106f0b68ad1138fb713394eb40da8
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-03 08:28:57 -0600

    SecondSection

--- src/components/DreamHouse/components/SecondSection.jsx
+++ src/components/DreamHouse/components/SecondSection.jsx
@@ -47,7 +47,23 @@ const SecondSection = () => {
               </p>
             </div>
 
-            <button>see Details</button>
+            <button className="flex flex-row m-auto font-bold text-blue-400 transition-all duration-700 ease-out hover:text-blue-600 ">
+              See Details{" "}
+              <svg
+                class="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
+                data-slot="icon"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  clip-rule="evenodd"
+                  d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+                  fill-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
           </div>
         </div>
       </div>
