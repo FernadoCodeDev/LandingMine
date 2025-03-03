604e275b721220fe7282afe2c2f4c7cfa85840ad
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-03 08:50:07 -0600

    SecondSection

--- src/components/DreamHouse/components/SecondSection.jsx
+++ src/components/DreamHouse/components/SecondSection.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import imgN1 from "../img/imgN1.webp";
+import imgN2 from "../img/imgN2.webp";
+import imgN3 from "../img/imgN3.webp";
 import Location from "./SVGComponents/Location";
 import Bed from "./SVGComponents/Bed";
 import Bathtub from "./SVGComponents/Bathtub";
@@ -36,7 +38,7 @@ const SecondSection = () => {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 mt-6 m-auto max-w-[70rem] md:grid-cols-3">
+      <div className="grid grid-cols-1 mt-6 m-auto max-w-[70rem] md:grid-cols-3 gap-4">
         <div className="bg-white rounded-lg shadow-md dark:bg-neutral-800">
           <img src={imgN1} alt="imgN1" className="w-full h-auto" />
           <div className="p-4">
@@ -53,17 +55,135 @@ const SecondSection = () => {
             <div className="flex flex-row justify-between gap-2 mt-4 text-black dark:text-white">
               <div className="flex flex-row gap-2">
                 <Bed width="20" />
-                <p className="font-bold text-slate-600 dark:text-slate-300">2</p>
+                <p className="font-bold text-slate-600 dark:text-slate-300">
+                  2
+                </p>
               </div>
 
               <div className="flex flex-row gap-2">
                 <Bathtub width="20" />
-                <p className="font-bold text-slate-600 dark:text-slate-300">2</p>
+                <p className="font-bold text-slate-600 dark:text-slate-300">
+                  2
+                </p>
               </div>
 
               <div className="flex flex-row">
                 <CurrencySVG width="20" />
-                <p className="font-bold text-slate-600 dark:text-slate-300">7,720,000</p>
+                <p className="font-bold text-slate-600 dark:text-slate-300">
+                  7,720,000
+                </p>
+              </div>
+            </div>
+
+            <button className="flex flex-row m-auto mt-4 font-bold text-blue-400 transition-all duration-700 ease-out hover:text-blue-600 ">
+              See Details{" "}
+              <svg
+                class="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
+                data-slot="icon"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  clip-rule="evenodd"
+                  d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+                  fill-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-md dark:bg-neutral-800">
+          <img src={imgN2} alt="imgN2" className="w-full h-auto" />
+          <div className="p-4">
+            <h2 className="text-xl font-semibold dark:text-white">
+              The Queen Inside - Type 3
+            </h2>
+            <div className="flex flex-row gap-2 mt-4 text-black dark:text-white">
+              <Location width="20" />
+              <p className="text-slate-600 dark:text-slate-300">
+                Evergreen Chemists
+              </p>
+            </div>
+
+            <div className="flex flex-row justify-between gap-2 mt-4 text-black dark:text-white">
+              <div className="flex flex-row gap-2">
+                <Bed width="20" />
+                <p className="font-bold text-slate-600 dark:text-slate-300">
+                  2
+                </p>
+              </div>
+
+              <div className="flex flex-row gap-2">
+                <Bathtub width="20" />
+                <p className="font-bold text-slate-600 dark:text-slate-300">
+                  2
+                </p>
+              </div>
+
+              <div className="flex flex-row">
+                <CurrencySVG width="20" />
+                <p className="font-bold text-slate-600 dark:text-slate-300">
+                  7,720,000
+                </p>
+              </div>
+            </div>
+
+            <button className="flex flex-row m-auto mt-4 font-bold text-blue-400 transition-all duration-700 ease-out hover:text-blue-600 ">
+              See Details{" "}
+              <svg
+                class="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
+                data-slot="icon"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  clip-rule="evenodd"
+                  d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+                  fill-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-md dark:bg-neutral-800">
+          <img src={imgN3} alt="imgN3" className="w-full h-auto" />
+          <div className="p-4">
+            <h2 className="text-xl font-semibold dark:text-white">
+              The Queen Inside - Type 3
+            </h2>
+            <div className="flex flex-row gap-2 mt-4 text-black dark:text-white">
+              <Location width="20" />
+              <p className="text-slate-600 dark:text-slate-300">
+                Evergreen Chemists
+              </p>
+            </div>
+
+            <div className="flex flex-row justify-between gap-2 mt-4 text-black dark:text-white">
+              <div className="flex flex-row gap-2">
+                <Bed width="20" />
+                <p className="font-bold text-slate-600 dark:text-slate-300">
+                  2
+                </p>
+              </div>
+
+              <div className="flex flex-row gap-2">
+                <Bathtub width="20" />
+                <p className="font-bold text-slate-600 dark:text-slate-300">
+                  2
+                </p>
+              </div>
+
+              <div className="flex flex-row">
+                <CurrencySVG width="20" />
+                <p className="font-bold text-slate-600 dark:text-slate-300">
+                  7,720,000
+                </p>
               </div>
             </div>
 
