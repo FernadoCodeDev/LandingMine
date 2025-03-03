4578747782bcd9ae674284f890aedaea080a1722
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-03 07:59:49 -0600

    Correction First Section

--- src/components/DreamHouse/components/SecondSection.jsx
+++ src/components/DreamHouse/components/SecondSection.jsx
@@ -1,8 +1,57 @@
 import React from "react";
+import imgN1 from "../img/imgN1.webp";
+import Location from "./SVGComponents/Location";
 
 const SecondSection = () => {
   return (
-    <div className="relative w-full p-4 text-black border-t-2 border-b-2 border-gray-300 dark:text-white min-h-auto bg-yellow-50 dark:bg-neutral-800"></div>
+    <div className="relative w-full min-h-screen p-4 text-black bg-white dark:text-white dark:bg-neutral-800">
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-2 m-auto max-w-[70rem]">
+        <h1 className="m-auto text-2xl font-bold">Popular Property</h1>
+
+        <p className="text-start text-slate-800 dark:text-gray-200">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum enim
+          nemo necessitatibus neque optio hic recusandae cum soluta sed aliquid
+          nihil explicabo, dignissimos velit provident? Beatae ab non error
+          officiis.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 mt-6 md:grid-cols-2 gap-2 m-auto max-w-[70rem]">
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-3">
+          <button className="p-2 bg-blue-400">Residencial</button>
+          <button className="p-2 text-black transition-all duration-700 ease-out border border-gray-300 bg-slate-100 hover:bg-slate-300">
+            Residencial
+          </button>
+
+          <button className="p-2 text-black transition-all duration-700 ease-out border border-gray-300 bg-slate-100 hover:bg-slate-300">
+            Residencial
+          </button>
+        </div>
+
+        <button className="w-full p-2 text-white transition-all duration-700 ease-out border md:ms-auto md:w-1/2 bg-neutral-900 hover:bg-black">
+          Residencial
+        </button>
+      </div>
+
+      <div className="grid grid-cols-1 mt-6 m-auto max-w-[70rem] md:grid-cols-3">
+        <div className="bg-gray-200 rounded-lg shadow-md dark:bg-neutral-800">
+          <img src={imgN1} alt="imgN1" className="w-full h-auto" />
+          <div className="p-4">
+            <h2 className="text-xl font-semibold dark:text-white">
+             The Queen Inside - Type 3
+            </h2>
+            <div className="flex flex-row justify-center">
+            <Location width="20"/>
+            <p className="text-black dark:text-white">
+            E-commerce sobre una tienda de articulos de cocina
+            </p>
+            </div>
+
+            <button>see Details</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
