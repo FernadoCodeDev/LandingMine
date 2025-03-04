6dd466ba21a4b2af191ff561dc2db3f1da3816c4
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-04 15:26:59 -0600

    SecondSection

--- src/components/WebCall/components/SecondSection.jsx
+++ src/components/WebCall/components/SecondSection.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import PersonN1 from "../img/PersonN1.webp";
+import PersonN2 from "../img/PersonN2.webp";
+import PersonN3 from "../img/PersonN3.webp";
+import PersonN4 from "../img/PersonN4.webp";
 
 const SecondSection = () => {
   return (
     <div className="relative w-full min-h-screen p-2 text-gray-900 bg-purple-300">
       <div className="pt-20 md:pt-32 m-auto flex flex-row justify-between w-full border-b-2 my-2 border-gray-900 p-2 max-w-[100rem]">
-        <p className="text-5xl font-fantasy ">SCHEDULE</p>
+        <p className="text-5xl font-fantasy ">HORARIO</p>
 
         <div className="flex flex-row items-end justify-end gap-2 ]">
-          <p className="text-xl ">VIEW ALL SCHEDULE</p>
+          <p className="text-xl ">VER TODO EL HORARIO</p>
           <div className="flex flex-col items-center justify-center w-8 h-8 p-1 border border-gray-900 rounded-full cursor-pointer">
             <svg
               class="w-8 h-8 transition-transform duration-500 group-hover:translate-x-1"
@@ -30,18 +34,18 @@ const SecondSection = () => {
       <div className="m-auto max-w-[100rem] grid grid-cols-1 md:grid-cols-3">
         <div className="grid grid-cols-3 md:col-start-3 justify-self-end">
           <div className="flex flex-col p-2 bg-amber-50">
-            <h1 className="text-purple-300 font-fantasy md:text-3xl">DAY 01</h1>
-            <p className="text-purple-300 ">12 March - Thursday</p>
+            <h1 className="text-purple-300 font-fantasy md:text-3xl">DÍA 01</h1>
+            <p className="text-purple-300 ">12 de marzo - jueves</p>
           </div>
 
           <div className="flex flex-col p-2 border border-amber-50">
-            <h1 className="text-amber-50 font-fantasy md:text-3xl">DAY 02</h1>
-            <p className="text-amber-50 ">14 March - Saturday</p>
+            <h1 className="text-amber-50 font-fantasy md:text-3xl">DÍA 02</h1>
+            <p className="text-amber-50 ">14 de marzo - sábado</p>
           </div>
 
           <div className="flex flex-col p-2 border border-amber-50">
-            <h1 className="text-amber-50 font-fantasy md:text-3xl">DAY 01</h1>
-            <p className="text-amber-50 ">16 March - Sunday</p>
+            <h1 className="text-amber-50 font-fantasy md:text-3xl">DÍA 01</h1>
+            <p className="text-amber-50 ">16 de marzo - domingo</p>
           </div>
         </div>
       </div>
@@ -52,7 +56,7 @@ const SecondSection = () => {
             9:00
           </h1>
           <h1 className="font-sans font-bold md:text-3xl text-amber-50">
-            DOORS OPEN
+          APERTURA
           </h1>
         </div>
 
@@ -61,30 +65,84 @@ const SecondSection = () => {
             9:30
           </h1>
           <h1 className="font-sans font-bold md:text-3xl text-amber-50">
-            INTRO & WELCOME
+          INTRODUCCIÓN Y BIENVENIDA
           </h1>
         </div>
 
         <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-5 bg-amber-50">
-          <h1 className="font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
+          <h1 className="flex flex-col items-center justify-center font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
             10:00
           </h1>
-          <h1 className="font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
-            T SHAPE DESIGNER
+          <h1 className="flex flex-col items-center justify-center font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
+          DISEÑADOR T-shaped
           </h1>
-          <p className="text-center ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            blanditiis assumenda cupiditate facilis omnis, repellendus
-            reiciendis aliquid vero aut vel.
+          <p className="flex flex-col items-center justify-center text-center ">
+          Un diseñador T-shaped es alguien que tiene un conocimiento profundo y específico dentro de su campo.
           </p>
 
-          <div className="w-4 h-4 bg-red-600 rounded-full"></div>
+          <div className="flex flex-col items-center justify-center">
+            <img src={PersonN1} alt="" className="w-40 h-auto rounded-full" />
+          </div>
 
           <div className="flex flex-col justify-center col-span-2 text-center md:col-span-1">
-          <h1 className="font-sans font-bold text-purple-400 md:text-3xl">
-            NAME LAST NAME
+            <h1 className="font-sans font-bold text-purple-400 md:text-3xl">
+              Esther Howard
+            </h1>
+            <p>Diseñador de productos</p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-5 bg-amber-50">
+          <h1 className="flex flex-col items-center justify-center font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
+            11:00
           </h1>
-          <p>DESCRIPTION</p>
+          <h1 className="flex flex-col items-center justify-center font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
+          MODELO DE FLEX BOX 
+          </h1>
+          <p className="flex flex-col items-center justify-center text-center ">
+          El modelo FLEX BOX en CS5 es un modo de diseño unidimensional
+          </p>
+
+          <div className="flex flex-col items-center justify-center">
+            <img
+              src={PersonN2}
+              alt="PersonN2"
+              className="w-40 h-auto rounded-full"
+            />
+          </div>
+
+          <div className="flex flex-col justify-center col-span-2 text-center md:col-span-1">
+            <h1 className="font-sans font-bold text-purple-400 md:text-3xl">
+              Jane Cooper
+            </h1>
+            <p>Desarrollador web</p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-5 bg-amber-50">
+          <h1 className="flex flex-col items-center justify-center font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
+            12:00
+          </h1>
+          <h1 className="flex flex-col items-center justify-center font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
+            DEEP WORK
+          </h1>
+          <p className="flex flex-col items-center justify-center text-center ">
+          Proceso de planificación y control consciente del tiempo dedicado a actividades específicas
+          </p>
+
+          <div className="flex flex-col items-center justify-center">
+            <img
+              src={PersonN3}
+              alt="PersonN3"
+              className="w-40 h-auto rounded-full"
+            />
+          </div>
+
+          <div className="flex flex-col justify-center col-span-2 text-center md:col-span-1">
+            <h1 className="font-sans font-bold text-purple-400 md:text-3xl">
+              Rachel Howard
+            </h1>
+            <p>React Developer</p>
           </div>
         </div>
 
@@ -95,6 +153,33 @@ const SecondSection = () => {
           <h1 className="font-sans font-bold md:text-3xl text-amber-50">
             LUNCH BREAK
           </h1>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-5 bg-amber-50">
+          <h1 className="flex flex-col items-center justify-center font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
+            14:00
+          </h1>
+          <h1 className="flex flex-col items-center justify-center font-sans font-bold text-center text-purple-400 md:text-start md:text-3xl">
+          INSPIRACIÓN DE DISEÑO
+          </h1>
+          <p className="flex flex-col items-center justify-center text-center ">
+          La inspiración puede venir de cualquier lugar, de un verso de poesía o del color de una acera.
+          </p>
+
+          <div className="flex flex-col items-center justify-center">
+            <img
+              src={PersonN4}
+              alt="PersonN4"
+              className="w-40 h-auto rounded-full"
+            />
+          </div>
+
+          <div className="flex flex-col justify-center col-span-2 text-center md:col-span-1">
+            <h1 className="font-sans font-bold text-purple-400 md:text-3xl">
+              Guy Hawkins
+            </h1>
+            <p>Director de arte</p>
+          </div>
         </div>
       </div>
     </div>
