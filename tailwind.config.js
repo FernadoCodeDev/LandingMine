7e437d92aa8073c7e7eb20861b73434f03dcfdcd
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-06 13:52:29 -0600

    Beige color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: 'class', 
+  darkMode: "class",
   theme: {
     extend: {
+      beige: {  // Beige color for the Morgana landing
+        50: "#f3efe6",
+        100: "#ede7db",
+        200: "#e2d9c8",
+        300: "#d6cbb9",
+        400: "#cfc4b0",
+        500: "#c1b39e",
+        600: "#b9ad93",
+        700: "#a49580",
+        800: "#9c8e74",
+        900: "#8b7b67",
+      },
       fontFamily: {
-        fantasy: ['Fantasy', 'cursive'], //Fantasy font for the landing Web Call
-        antonio: ['Antonio'], //Antonio font for the landing WPL
+        fantasy: ["Fantasy", "cursive"], //Fantasy font for the landing Web Call
+        antonio: ["Antonio"], //Antonio font for the landing WPL
       },
     },
   },
   plugins: [],
-}
-
+};
