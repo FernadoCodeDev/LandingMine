6977daed20b6659a1bfeaaff7e10ff91a3032565
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-06 09:32:27 -0600

    SecondSection

--- src/components/WLP/components/SecondSection.jsx
+++ src/components/WLP/components/SecondSection.jsx
@@ -2,7 +2,47 @@ import React from "react";
 
 const SecondSection = () => {
   return (
-    <div className="relative w-full min-h-screen p-2 bg-gray-900 text-amber-50"></div>
+    <div className="flex items-center justify-center relative w-full min-h-screen  bg-orange-100  dark:bg-cyan-950 text-amber-50 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center m-auto max-w-[70rem] gap-4 md:gap-12">
+        <div className="grid grid-cols-1  ">
+          <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-7xl md:text-9xl">
+            1,000
+          </h1>
+          <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-4xl md:text-6xl">
+          Campeones siendo formados en distintas ciudades
+          </h1>
+        </div>
+
+        <div className="grid grid-cols-1  ">
+          <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-7xl md:text-9xl">
+            10,000
+          </h1>
+          <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-4xl md:text-6xl">
+          Luchadores forjados en cada ring
+          </h1>
+        </div>
+
+        <div className="grid grid-cols-1  ">
+          <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-7xl md:text-9xl">
+            500
+          </h1>
+          <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-4xl md:text-6xl">
+          Victorias en torneos y exhibiciones
+          </h1>
+        </div>
+
+        <div className="grid grid-cols-1  ">
+          <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-7xl md:text-9xl">
+            +1,000
+          </h1>
+          <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-4xl md:text-6xl">
+          Batallas ganadas con honor y pasión
+          </h1>
+        </div>
+
+      
+      </div>
+    </div>
   );
 };
 
