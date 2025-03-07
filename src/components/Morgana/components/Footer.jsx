ee0f2eea1bd88a4f2e98346100a61edd222a67e6
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-07 10:55:03 -0600

    FooterMorgana

--- src/components/Morgana/components/Footer.jsx
+++ src/components/Morgana/components/Footer.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full p-6 text-white bg-neutral-900">
+    <footer className="w-full p-6 text-white bg-beige-900">
       <div className="max-w-6xl mx-auto">
         <p className="mb-4 text-lg leading-relaxed">
-        <span className="font-bold">Dream House</span> expresa su más sincero agradecimiento al talentoso diseñador
+        <span className="font-bold">Morgana</span> expresa su más sincero agradecimiento al talentoso diseñador
    {" "}
           <a
-            href="https://dribbble.com/MahmuduR"
-            className="ml-1 font-bold text-white transition duration-300 ease-in-out hover:text-blue-400"
+            href="https://dribbble.com/hello1usman"
+            className="ml-1 font-bold text-white transition duration-300 ease-in-out hover:text-beige-400"
           >
-            Mahmudur Rahman
+           Usman A
           </a>{" "}
           cuya obra sirvió de inspiración para la creación de la landing page{" "}
-          <span className="font-bold">Dream House</span>. Utilicé su diseño como
+          <span className="font-bold">Morgana</span>. Utilicé su diseño como
           referencia para desarrollar esta interpretación propia, respetando
           siempre su estilo.
         </p>
 
         <p className="mb-4 text-lg leading-relaxed">
-          Cabe aclarar que <span className="font-bold">Dream House</span> no es
+          Cabe aclarar que <span className="font-bold">Morgana</span> no es
           una Agencia real y fue creado únicamente con fines de
           desarrollo y diseño. Además, yo diseñé el logotipo de{" "}
-          <span className="font-bold">Dream House</span> y utilicé imágenes de
+          <span className="font-bold">Morgana</span> y utilicé imágenes de
           uso libre en su construcción.
         </p>
 
@@ -33,13 +33,13 @@ const Footer = () => {
         </p>
       </div>
 
-      <div className="mt-8 border-t-2 border-black opacity-50"></div>
+      <div className="mt-8 border-t-2 opacity-50 border-beige-400"></div>
 
       <p className="mt-6 text-sm text-center">
-        © 2025 Dream House.{" "}
+        © 2025 Morgana.{" "}
         <a
           href="/"
-          className="ml-1 font-bold text-white transition duration-300 ease-in-out hover:text-blue-400"
+          className="ml-1 font-bold text-white transition duration-300 ease-in-out hover:text-beige-400"
         >
           Regresar a LandingMine
         </a>
