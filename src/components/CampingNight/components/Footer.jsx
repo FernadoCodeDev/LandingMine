2970b1de022e307c6e442a1ea0c11b6015dc5e26
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-09 16:02:40 -0600

    FooterCN

--- src/components/CampingNight/components/Footer.jsx
+++ src/components/CampingNight/components/Footer.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full p-6 text-white bg-neutral-900">
+    <footer className="w-full p-6 text-black bg-beige-200">
       <div className="max-w-6xl mx-auto">
         <p className="mb-4 text-lg leading-relaxed">
-          <span className="font-bold">Dream House</span> expresa su más sincero
+          <span className="font-bold">Camping Night</span> expresa su más sincero
           agradecimiento al talentoso diseñador{" "}
           <a
-            href="https://dribbble.com/MahmuduR"
-            className="ml-1 font-bold text-white transition duration-300 ease-in-out hover:text-blue-400"
+            href="https://dribbble.com/uiamir36"
+            className="ml-1 font-bold transition duration-300 ease-in-out hover:text-yellow-900"
           >
-            Mahmudur Rahman
+            Amirul Islam
           </a>{" "}
           cuya obra sirvió de inspiración para la creación de la landing page{" "}
-          <span className="font-bold">Dream House</span>. Utilicé su diseño como
+          <span className="font-bold">Camping Night</span>. Utilicé su diseño como
           referencia para desarrollar esta interpretación propia, respetando
           siempre su estilo.
         </p>
 
         <p className="mb-4 text-lg leading-relaxed">
-          Cabe aclarar que <span className="font-bold">Dream House</span> no es
+          Cabe aclarar que <span className="font-bold">Camping Night</span> no es
           una Agencia real y fue creado únicamente con fines de desarrollo y
           diseño. Además, yo diseñé el logotipo de{" "}
-          <span className="font-bold">Dream House</span> y utilicé imágenes de
+          <span className="font-bold">Camping Night</span> y utilicé imágenes de
           uso libre en su construcción.
         </p>
 
@@ -36,10 +36,10 @@ const Footer = () => {
       <div className="mt-8 border-t-2 border-black opacity-50"></div>
 
       <p className="mt-6 text-sm text-center">
-        © 2025 Dream House.{" "}
+        © 2025 Camping Night.{" "}
         <a
           href="/"
-          className="ml-1 font-bold text-white transition duration-300 ease-in-out hover:text-blue-400"
+          className="ml-1 font-bold text-black transition duration-300 ease-in-out hover:text-yellow-900"
         >
           Regresar a LandingMine
         </a>
