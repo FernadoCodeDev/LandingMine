4194d853c9219b1c1f4ac2119c986091ae8284f1
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-09 09:05:46 -0600

    Tooth3SVG

--- src/components/GoldTeeth/components/SVGComponents/Tooth3SVG.jsx
+++ src/components/GoldTeeth/components/SVGComponents/Tooth3SVG.jsx
@@ -22,7 +22,7 @@ const Tooth3SVG = (props) => {
         d="M16,3a3.84,3.84,0,0,0-2.32.74,2.88,2.88,0,0,1-3.36,0A3.84,3.84,0,0,0,8,3C5,3,3,5,4,10S5,21,8,21s2-6.76,4-6.76S13,21,16,21s3-6,4-11S19,3,16,3Z"
         style={{
           fill: "none",
-          stroke: "#8b0836",
+          stroke: "#ede7db",
           strokeLinecap: "round",
           strokeLinejoin: "round",
           strokeWidth: 2,
