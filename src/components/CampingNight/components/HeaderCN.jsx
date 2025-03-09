4f875352c16eceaa56dd66eb6b077ad38ff99cb8
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-09 15:24:52 -0600

    HeaderCN

--- src/components/CampingNight/components/HeaderCN.jsx
+++ src/components/CampingNight/components/HeaderCN.jsx
@@ -13,11 +13,11 @@ const HeaderDH = () => {
       ></div>
 
       <div className="relative grid items-start h-screen grid-cols-2 gap-2 p-4 md:grid-cols-3">
-        <div className="order-3 col-span-2 mx-auto md:col-span-1 md:order-2 md:w-auto md:h-20 ">
+        <div className="order-3 col-span-2 mx-auto md:col-span-1 md:order-2 md:w-auto md:h-20 md:bg-white/20 md:bg-backdrop-blur-[2px]">
           <MobileMenu
-            textColor="text-black transition-all duration-700 ease-out"
-            hoverColor="hover:text-gray-800"
-            bgColor="bg-backdrop-blur-[4px] last-of-type:rounded-b-3xl bg-black/20 md:bg-transparent"
+            textColor="text-white transition-all duration-700 ease-out"
+            hoverColor="hover:text-orange-300"
+            bgColor="bg-backdrop-blur-[2px] last-of-type:rounded-b-3xl bg-white/20 md:bg-transparent"
             invertColor="invert-0"
           />
         </div>
@@ -25,15 +25,54 @@ const HeaderDH = () => {
         <img
           src={CampingNightLogo}
           alt="CampingNightLogo"
-          className="order-1 object-contain w-40 h-auto mx-auto cursor-pointer md:order-1"
+          className="order-1 max-w-[15rem] h-auto mx-auto cursor-pointer md:order-1"
         />
 
-        <div className=" w-fit ms-auto order-2 md:order-3 backdrop-blur-[4px] bg-black/20 p-2 rounded-3xl">
+        <div className=" w-fit ms-auto order-2 md:order-3 backdrop-blur-[4px] bg-white/20 p-2 rounded-3xl">
           <DarkMode />
         </div>
 
-        <div className="relative order-4 col-span-2 text-center md:col-span-3">
-         
+        <div className="relative order-4 col-span-2 md:col-span-3">
+          <div className="m-auto grid grid-cols-1 gap-4 md:grid-cols-2 max-w-[90rem] p-4">
+            <h1 className="text-4xl text-white md:text-6xl lg:text-8xl">
+              Un Hermoso lugar con un rico{" "}
+              <span className="font-bold">S'more</span>
+            </h1>
+
+            <div className="m-auto w-full p-4 bg-white max-w-[70rem]">
+              <div className="grid grid-cols-1 gap-2">
+                <div className="flex flex-col">
+                  <h1 className="font-bold text-start">Fecha de llegada</h1>
+                  <input className="" type="date" />
+                </div>
+
+                <div className="flex flex-col">
+                  <h1 className="font-bold text-start">Tipo de campamento</h1>
+                  <select className="">
+                    <option>Espacio para tienda</option>
+                    <option>Cabaña rústica</option>
+                    <option>Zona con fogata</option>
+                  </select>
+                </div>
+
+                <div className="flex flex-col">
+                  <h1 className="font-bold text-start">
+                    Servicios adicionales
+                  </h1>
+                  <select className="">
+                    <option>Ninguno</option>
+                    <option>Renta de equipo</option>
+                    <option>Guía turístico</option>
+                    <option>Leña extra</option>
+                  </select>
+                </div>
+
+                <button className="flex justify-center w-full h-auto p-2 font-bold text-white transition-all duration-700 ease-out bg-orange-400 hover:bg-orange-600">
+                  Reservar Ahora
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
