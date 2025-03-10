078e651fc56f40768cdec4bc887bcde547c0fd99
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-10 15:19:13 -0600

    HeaderC

--- src/components/Clover/components/HeaderClover.jsx
+++ src/components/Clover/components/HeaderClover.jsx
@@ -1,32 +1,81 @@
 import React from "react";
 import CloverLogo from "../img/CloverLogo.webp";
+import imgN1 from "../img/imgN1.webp";
 import MobileMenu from "../../GlobalComponents/MobileMenu";
 import DarkMode from "../../GlobalComponents/DarkMode";
 
 const HeaderClover = () => {
   return (
-    <div className="relative w-full min-h-screen bg-sky-800">
-      <div className="relative grid items-start h-screen grid-cols-2 gap-2 p-4 md:grid-cols-3">
-        <div className="order-3 col-span-2 mx-auto md:col-span-1 md:order-2 md:w-auto md:h-20 md:bg-white/20 md:bg-backdrop-blur-[2px]">
-          <MobileMenu
-            textColor="text-white transition-all duration-700 ease-out"
-            hoverColor="hover:text-orange-300"
-            bgColor="bg-backdrop-blur-[2px] last-of-type:rounded-b-3xl bg-white/20 md:bg-transparent"
-            invertColor="invert-0"
+    <div className="text-white lg:text-2xl relative grid items-start h-screen grid-cols-2 gap-2  md:grid-cols-3 w-full min-h-screen bg-sky-800">
+      <div className="mt-4 order-3 col-span-2 mx-auto md:col-span-1 md:order-2 md:w-auto md:h-20 md:bg-white/20 md:bg-backdrop-blur-[2px]">
+        <MobileMenu
+          textColor="text-white transition-all duration-700 ease-out"
+          hoverColor="hover:text-lime-400"
+          bgColor="bg-backdrop-blur-[2px] last-of-type:rounded-b-3xl bg-white/20 md:bg-transparent"
+          invertColor="invert-0"
+        />
+      </div>
+
+      <img
+        src={CloverLogo}
+        alt="CloverLogo"
+        className="p-4  order-1 max-w-[15rem] h-auto mx-auto cursor-pointer md:order-1"
+      />
+
+      <div className="p-4  w-fit ms-auto order-2 md:order-3">
+        <DarkMode />
+      </div>
+
+      <div className="ms-auto grid grid-cols-1 gap-4 md:grid-cols-2 max-w-[100rem] relative order-4 col-span-2 md:col-span-3">
+        <div className="p-4  flex flex-col items-center gap-3">
+          <h1 className="font-bold text-2xl lg:text-5xl">
+            Make green your favorite home &
+            <span className="font-bold text-lime-400"> Living better</span>
+          </h1>
+
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,
+            sapiente? Nesciunt minima aspernatur voluptatibus, tempore molestias
+            itaque impedit eum quis aperiam atque eius. Facilis quas esse, animi
+            beatae quidem consequatur?
+          </p>
+
+          <div className="grid grid-cols-2 gap-4">
+            <button className="flex justify-center w-full p-2 font-bold text-white transition-all duration-700 ease-out bg-lime-500 hover:bg-lime-600">
+              Shop Now
+            </button>
+
+            <button className="flex justify-center w-full p-2 font-bold text-white transition-all duration-700 ease-out bg-lime-500 hover:bg-lime-600">
+              Explore Now
+            </button>
+          </div>
+
+          <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 gap-2">
+              <h1 className="font-bold text-lime-400">250+</h1>
+              <p>Order Everyday</p>
+            </div>
+
+            <div className="grid grid-cols-1 gap-2">
+              <h1 className="font-bold text-lime-400">250+</h1>
+              <p>Order Everyday</p>
+            </div>
+
+            <div className="grid grid-cols-1 gap-2">
+              <h1 className="font-bold text-lime-400">250+</h1>
+              <p>Order Everyday</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="hidden md:block ">
+          <img
+            src={imgN1}
+            alt="imgN1"
+            className="ms-auto max-w-[30rem] h-auto rounded-tl-full"
+            style={{ maskImage: "linear-gradient(black 90%, transparent)" }}
           />
         </div>
-
-        <img
-          src={CloverLogo}
-          alt="CloverLogo"
-          className="order-1 max-w-[15rem] h-auto mx-auto cursor-pointer md:order-1"
-        />
-
-        <div className=" w-fit ms-auto order-2 md:order-3 backdrop-blur-[4px] bg-white/20 p-2 rounded-3xl">
-          <DarkMode />
-        </div>
-
-        <div className="relative order-4 col-span-2 md:col-span-3"></div>
       </div>
     </div>
   );
