57bfb0960d7dae8015d6de18b2b9d78fbcf6eb85
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-10 09:01:12 -0600

    FooterCN

--- src/components/CampingNight/components/Footer.jsx
+++ src/components/CampingNight/components/Footer.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full p-6 text-black bg-beige-200">
+    <footer className="w-full p-6 text-black dark:text-white bg-beige-200 dark:bg-black">
       <div className="max-w-6xl mx-auto">
         <p className="mb-4 text-lg leading-relaxed">
           <span className="font-bold">Camping Night</span> expresa su más sincero
           agradecimiento al talentoso diseñador{" "}
           <a
             href="https://dribbble.com/uiamir36"
-            className="ml-1 font-bold transition duration-300 ease-in-out hover:text-yellow-900"
+            className="ml-1 font-bold transition duration-300 ease-in-out hover:text-yellow-900 dark:hover:text-beige-500"
           >
             Amirul Islam
           </a>{" "}
@@ -33,13 +33,13 @@ const Footer = () => {
         </p>
       </div>
 
-      <div className="mt-8 border-t-2 border-black opacity-50"></div>
+      <div className="mt-8 border-t-2 border-black opacity-50 dark:border-white"></div>
 
       <p className="mt-6 text-sm text-center">
         © 2025 Camping Night.{" "}
         <a
           href="/"
-          className="ml-1 font-bold text-black transition duration-300 ease-in-out hover:text-yellow-900"
+          className="ml-1 transition duration-300 ease-in-out font-bol hover:text-yellow-900 dark:hover:text-beige-500"
         >
           Regresar a LandingMine
         </a>
