4217039a6fa27ec799e746d31817a953372d0e66
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-10 17:19:59 -0600

    FooterClover

--- src/components/Clover/components/Footer.jsx
+++ src/components/Clover/components/Footer.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full p-6 text-black dark:text-white bg-beige-200 dark:bg-black">
+    <footer className="w-full p-6 text-white bg-sky-900">
       <div className="max-w-6xl mx-auto">
         <p className="mb-4 text-lg leading-relaxed">
-          <span className="font-bold">Camping Night</span> expresa su más
+          <span className="font-bold">Clover</span> expresa su más
           sincero agradecimiento al talentoso diseñador{" "}
           <a
-            href="https://dribbble.com/uiamir36"
-            className="ml-1 font-bold transition duration-300 ease-in-out hover:text-yellow-900 dark:hover:text-beige-500"
+            href="https://dribbble.com/nayonchandraroy123"
+            className="ml-1 font-bold transition duration-300 ease-in-out hover:text-lime-400 "
           >
-            Amirul Islam
+            Nayon Roy
           </a>{" "}
           cuya obra sirvió de inspiración para la creación de la landing page{" "}
-          <span className="font-bold">Camping Night</span>. Utilicé su diseño
+          <span className="font-bold">Clover</span>. Utilicé su diseño
           como referencia para desarrollar esta interpretación propia,
           respetando siempre su estilo.
         </p>
 
         <p className="mb-4 text-lg leading-relaxed">
-          Cabe aclarar que <span className="font-bold">Camping Night</span> no
+          Cabe aclarar que <span className="font-bold">Clover</span> no
           es una Agencia real y fue creado únicamente con fines de desarrollo y
           diseño. Además, yo diseñé el logotipo de{" "}
-          <span className="font-bold">Camping Night</span> y utilicé imágenes de
+          <span className="font-bold">Clover</span> y utilicé imágenes de
           uso libre en su construcción.
         </p>
 
@@ -33,13 +33,13 @@ const Footer = () => {
         </p>
       </div>
 
-      <div className="mt-8 border-t-2 border-black opacity-50 dark:border-white"></div>
+      <div className="mt-8 border-t-2 border-lime-400 opacity-50 "></div>
 
       <p className="mt-6 text-sm text-center">
-        © 2025 Camping Night.{" "}
+        © 2025 Clover.{" "}
         <a
           href="/"
-          className="ml-1 transition duration-300 ease-in-out font-bol hover:text-yellow-900 dark:hover:text-beige-500"
+          className="ml-1 transition duration-300 ease-in-out font-bol hover:text-lime-400 "
         >
           Regresar a LandingMine
         </a>
