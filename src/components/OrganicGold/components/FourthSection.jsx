917b3f4c45dfac588bcff0f60b4c1e018ec9c888
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-11 14:08:41 -0600

    Fourth section

--- src/components/OrganicGold/components/FourthSection.jsx
+++ src/components/OrganicGold/components/FourthSection.jsx
@@ -5,20 +5,29 @@ const FourthSection = () => {
     <div className="relative w-full p-4 md:text-2xl text-black bg-white dark:text-white min-h-auto dark:bg-neutral-900">
       <div className="grid grid-cols-1 gap-3 m-auto max-w-[70rem] ">
         <div className="flex bg-yellow-300  rounded-xl">
-          <div className=" flex flex-col items-center  justify-center  gap-4 text-black p-6 m-auto">
-            <h1 className="text-2xl md:text-5xl max-w-[30rem] text-center  font-bold">
-              TEXT
+          <div className=" flex flex-col items-center justify-center  gap-6 text-black p-6 m-auto">
+            <h1 className="text-2xl md:text-4xl max-w-[30rem] text-center  font-bold">
+              Cultivamos calidad, cosechamos bienestar
             </h1>
 
-            <p className=" max-w-[30rem] text-start ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-              enim omnis incidunt quam, in sint, nam cupiditate amet,
-              dignissimos ut sunt neque totam itaque doloremque perferendis.
-              Cupiditate aut aliquid provident.
+            <p className=" max-w-[30rem] text-center ">
+              En <span className="text-lime-600 font-bold">Organic Gold</span> ,
+              nuestra misión es conectar a las personas con lo mejor de la
+              naturaleza, ofreciendo productos orgánicos de la más alta calidad.
+              Creemos en un futuro más saludable y sostenible, donde cada
+              alimento respete el medio ambiente y beneficie tanto a quienes lo
+              producen como a quienes lo consumen.
+            </p>
+
+            <p className=" max-w-[30rem] text-center ">
+              A través de nuestra red de agricultores locales, garantizamos
+              productos frescos, de temporada y cultivados con prácticas
+              responsables. Queremos inspirar un estilo de vida más natural,
+              promoviendo el consumo consciente y el respeto por la tierra.
             </p>
 
             <button className="flex items-center justify-center text-xl  w-full md:w-1/2 p-2 font-bold transition-all duration-700 ease-out rounded-full bg-lime-300 hover:bg-lime-500">
-              Text{" "}
+              Ver más{" "}
               <svg
                 className="w-6 h-6 transition-transform duration-500 group-hover:translate-x-1"
                 data-slot="icon"
