f482a04aa5ced8b24aa58952f59a5d1514fa1958
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-13 11:17:49 -0600

    ThirdSection

--- src/components/ZoologicalWorld/components/ThirdSection.jsx
+++ src/components/ZoologicalWorld/components/ThirdSection.jsx
@@ -8,44 +8,61 @@ const ThirdSection = () => {
   return (
     <div className="relative flex flex-col items-center w-full min-h-screen gap-4 p-4 text-black bg-white md:text-2xl dark:text-white dark:bg-neutral-900">
       <h1 className="text-2xl font-bold text-center text-orange-500 md:text-4xl">
-        TEXT
+        Instalaciones de Zoological World
       </h1>
+      <p className="text-xl text-center md:text-2xl max-w-[50rem]">
+        En Zoological World, nos aseguramos de que tu experiencia sea cómoda y
+        memorable, con servicios y facilidades para todos los visitantes.
+      </p>
       <div className=" max-w-[80rem] grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="flex flex-col items-center gap-4">
-          <TrayFoodSVG       height="150"
-      width="150"/>
-          <h1 className="font-bold text-center">Text</h1>
-          <p className="text-center">Text</p>
+          <TrayFoodSVG height="150" width="150" />
+          <h1 className="font-bold text-center">Sabores de la Sabana</h1>
+          <p className="text-center">
+            Disfruta de una variedad de opciones gastronómicas que te harán
+            sentir como si estuvieras en la sabana. Desde platos rápidos hasta
+            opciones gourmet, tenemos algo para todos los gustos.
+          </p>
         </div>
 
         <div className="flex flex-col items-center gap-4">
-          <ParkingSVG  height="150"
-      width="150"/>
-          <h1 className="font-bold text-center">Text</h1>
-          <p className="text-center">Text</p>
+          <ParkingSVG height="150" width="150" />
+          <h1 className="font-bold text-center">
+            Tu comodidad, nuestra prioridad
+          </h1>
+          <p className="text-center">
+            Contamos con un amplio estacionamiento para que puedas disfrutar de
+            tu visita sin preocupaciones. ¡Estaciona cómodamente y adéntrate en
+            la aventura!
+          </p>
         </div>
       </div>
 
       <h1 className="text-2xl font-bold text-center text-orange-500 md:text-4xl">
-        TEXT
+        Horarios
       </h1>
+
       <div className=" max-w-[80rem] grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="flex flex-col items-center gap-4">
-          <GateSVG  height="150"
-      width="150"/>
-          <h1 className="font-bold text-center">Text</h1>
-          <p className="text-center">Text</p>
+          <GateSVG height="150" width="150" />
+          <h1 className="font-bold text-center">Cerrado los lunes</h1>
+          <p className="text-center">
+            Nuestro zoológico estará cerrado los lunes para mantenimiento y
+            cuidado de los animales. Te esperamos con los brazos abiertos el
+            resto de la semana.
+          </p>
         </div>
 
         <div className="flex flex-col items-center gap-4">
-          <OpenGateSVG  height="150"
-      width="150"/>
-          <h1 className="font-bold text-center">Text</h1>
-          <p className="text-center">Text</p>
+          <OpenGateSVG height="150" width="150" />
+          <h1 className="font-bold text-center">Abierto de martes a domingo</h1>
+          <p className="text-center">
+            Desde martes hasta domingo, nuestras puertas están abiertas para que
+            disfrutes de un día lleno de aventuras y aprendizaje. ¡No te lo
+            pierdas!
+          </p>
         </div>
       </div>
-
-    
     </div>
   );
 };
