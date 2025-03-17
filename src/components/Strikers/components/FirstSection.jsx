196404962bbf14114251ea79c342992d51fe5bf0
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-17 12:11:09 -0600

    FirstSection

--- src/components/Strikers/components/FirstSection.jsx
+++ src/components/Strikers/components/FirstSection.jsx
@@ -2,8 +2,68 @@ import React from "react";
 
 const FirstSection = () => {
   return (
-    <div className="relative flex flex-col items-center w-full min-h-screen text-black bg-white dark:bg-neutral-900 md:text-2xl dark:text-white">
-     
+    <div className="relative flex flex-col items-center w-full min-h-screen text-black bg-white border-t-2 border-orange-500 md:text-2xl dark:border-slate-950 dark:bg-slate-950 dark:text-white">
+      <div className="m-auto max-w-[90rem] p-4 grid grid-cols-1 md:grid-cols-3">
+        <div className="flex flex-col items-center gap-4 p-4">
+          <h1 className="font-bold text-orange-500">TEXT</h1>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            officia aliquid, quisquam reprehenderit cupiditate nostrum
+            consequuntur ipsam harum illo accusamus qui quibusdam, ad saepe.
+            Optio temporibus minima possimus voluptate omnis.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center gap-4 p-4 bg-orange-200 dark:bg-slate-900">
+          <h1 className="font-bold text-orange-500">TEXT</h1>
+          <p className="text-center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+            repellendus earum adipisci ipsa sed cupiditate aspernatur assumenda
+            iure molestiae libero exercitationem praesentium dolor laboriosam ut
+            ratione, eligendi sapiente expedita enim.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center gap-4 p-4">
+          <h1 className="font-bold text-orange-500">text</h1>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
+            adipisci laboriosam voluptas quas accusamus maiores quisquam odit
+            alias reprehenderit? Accusamus magni ipsam explicabo placeat quidem
+            magnam maxime ab suscipit facere!
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center gap-4 p-4 bg-orange-200 dark:bg-slate-900">
+          <h1 className="font-bold text-orange-500">TEXT</h1>
+          <p className="text-center">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+            dignissimos iure molestias officiis assumenda sed pariatur dolore
+            maiores reiciendis nostrum, facere sit, expedita repellat, cumque
+            praesentium? Dicta laborum eaque alias?
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center gap-4 p-4 ">
+          <h1 className="font-bold text-orange-500">TEXT</h1>
+          <p className="text-center">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+            dignissimos iure molestias officiis assumenda sed pariatur dolore
+            maiores reiciendis nostrum, facere sit, expedita repellat, cumque
+            praesentium? Dicta laborum eaque alias?
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center gap-4 p-4 bg-orange-200 dark:bg-slate-900">
+          <h1 className="font-bold text-orange-500">TEXT</h1>
+          <p className="text-center">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+            dignissimos iure molestias officiis assumenda sed pariatur dolore
+            maiores reiciendis nostrum, facere sit, expedita repellat, cumque
+            praesentium? Dicta laborum eaque alias?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
