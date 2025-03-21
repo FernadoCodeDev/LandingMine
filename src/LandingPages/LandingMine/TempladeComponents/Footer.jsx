7c5cf2e4f62a25a1943df3986e43cdff9567d72d
Fernando <fernando.e.mejia.e@gmail.com>
2025-03-21 09:15:41 -0600

    FooterLandingMine

--- src/LandingPages/LandingMine/TempladeComponents/Footer.jsx
+++ src/LandingPages/LandingMine/TempladeComponents/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="w-full py-4 text-center text-white bg-blue-950">
       <h3>
         Puedes encontrar este y más trabajos mios en 
-        <a href="#">
+        <a href="http://fercode.atwebpages.com/">
           <span className="ml-1 font-bold text-white transition duration-500 ease-in-out hover:text-orange-300">
              mí página personal
           </span>
