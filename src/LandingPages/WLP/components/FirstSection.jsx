67a31e888c1f36ac41ee5d4e0895d51212c50100
Fernando <fernando.e.mejia.e@gmail.com>
2025-04-08 15:13:47 -0600

    WLP Adjustment

--- src/LandingPages/WLP/components/FirstSection.jsx
+++ src/LandingPages/WLP/components/FirstSection.jsx
@@ -3,32 +3,32 @@ import imgN1 from "../img/imgN1.webp";
 
 const FirstSection = () => {
   return (
-    <div className="relative w-full min-h-screen  bg-orange-50  dark:bg-sky-900 text-amber-50">
-      <div className="bg-orange-100 dark:bg-cyan-950  flex flex-row justify-around p-2">
-        <h1 className="text-center text-black dark:text-white font-bold text-sm md:text-3xl">
+    <div className="relative flex flex-col items-center justify-center w-full h-auto gap-4 bg-orange-50 dark:bg-sky-900 text-amber-50">
+      <div className="flex flex-row justify-around w-full p-2 bg-orange-100 dark:bg-cyan-950">
+        <h1 className="text-sm font-bold text-center text-black dark:text-white md:text-3xl">
           NUEVOS CAMPEONES
         </h1>
-        <h1 className="text-center text-black dark:text-white font-bold text-sm md:text-3xl">
+        <h1 className="text-sm font-bold text-center text-black dark:text-white md:text-3xl">
           |
         </h1>
-        <h1 className="text-center text-black dark:text-white font-bold text-sm md:text-3xl">
+        <h1 className="text-sm font-bold text-center text-black dark:text-white md:text-3xl">
           NUEVOS RETADORES
         </h1>
-        <h1 className="text-center text-black dark:text-white font-bold text-sm md:text-3xl">
+        <h1 className="text-sm font-bold text-center text-black dark:text-white md:text-3xl">
           |
         </h1>
-        <h1 className="text-center text-black dark:text-white font-bold text-sm md:text-3xl">
+        <h1 className="text-sm font-bold text-center text-black dark:text-white md:text-3xl">
           NUEVOS DESAFÍOS
         </h1>
       </div>
 
-      <div className="p-2 grid grid-cols-1 md:grid-cols-2 gap-4 m-auto max-w-[70rem]">
-        <div className="order-3 md:order-1 flex flex-col justify-between gap-4">
+      <div className="p-4 grid grid-cols-1 md:grid-cols-2 gap-4 m-auto max-w-[80rem]">
+        <div className="flex flex-col justify-between order-3 gap-4 md:order-1">
           <div className="flex flex-col gap-4">
-            <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-3xl md:text-5xl">
+            <h1 className="text-3xl font-bold text-center text-cyan-950 dark:text-orange-50 md:text-5xl">
               Domina el Ring, Conquista tu Destino
             </h1>
-            <p className="text-start text-black dark:text-white  md:text-xl">
+            <p className="text-black text-start dark:text-white md:text-xl">
               En WLP llevamos la pasión por la lucha libre al siguiente nivel.
               Aprende las técnicas, mejora tu fuerza y desarrolla tu estilo
               único en un ambiente diseñado para verdaderos guerreros del ring.
@@ -37,7 +37,7 @@ const FirstSection = () => {
             </p>
           </div>
 
-          <button className=" m-auto w-full md:w-1/3 dark:text-black flex justify-center p-2 transition-all duration-700 ease-out bg-sky-900 dark:bg-orange-50 md:text-2xl font-antonio hover:bg-cyan-950 dark:hover:bg-orange-200">
+          <button className="flex justify-center w-full p-2 m-auto transition-all duration-700 ease-out md:w-1/3 dark:text-black bg-sky-900 dark:bg-orange-50 md:text-2xl font-antonio hover:bg-cyan-950 dark:hover:bg-orange-200">
             Leer Más
           </button>
         </div>
@@ -46,8 +46,8 @@ const FirstSection = () => {
           <img src={imgN1} alt="imgN1" className="w-full h-auto" />
         </div>
 
-        <div className="order-2 md:order-3 md:col-span-2 flex flex-col items-center gap-4">
-          <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-3xl md:text-5xl">
+        <div className="flex flex-col items-center order-2 gap-4 md:order-3 md:col-span-2">
+          <h1 className="text-3xl font-bold text-center text-cyan-950 dark:text-orange-50 md:text-5xl">
             Lucha con Honor, Gana con Pasión
           </h1>
           <p className="text-center text-black dark:text-white  md:text-xl max-w-[50rem]">
--- src/LandingPages/WLP/components/ThirdSection.jsx
+++ src/LandingPages/WLP/components/ThirdSection.jsx
@@ -3,81 +3,81 @@ import imgN2 from "../img/imgN2.webp";
 
 const ThirdSection = () => {
   return (
-    <div className="relative w-full min-h-screen  bg-orange-50  dark:bg-sky-900 text-amber-50">
-      <div className="p-2 grid grid-cols-1 md:grid-cols-2 gap-4 m-auto max-w-[70rem]">
-        <div className=" ">
-          <h1 className="text-center border-b-2 pb-2 border-cyan-950 dark:border-orange-50  text-cyan-950 dark:text-orange-50 font-bold text-4xl md:text-6xl">
+    <div className="relative flex flex-col items-center justify-center w-full h-auto gap-4 p-4 bg-orange-50 dark:bg-sky-900 text-amber-50">
+      <div className="p-2 grid grid-cols-1 md:grid-cols-2 gap-4 m-auto max-w-[80rem]">
+        <div className="">
+          <h1 className="pb-2 text-4xl font-bold text-center border-b-2 border-cyan-950 dark:border-orange-50 text-cyan-950 dark:text-orange-50 md:text-6xl">
             HONOR
           </h1>
 
-          <h1 className="text-center   text-cyan-950 dark:text-orange-50 font-bold text-3xl md:text-5xl">
+          <h1 className="text-3xl font-bold text-center text-cyan-950 dark:text-orange-50 md:text-5xl">
             CONSTANCIA
           </h1>
           <img src={imgN2} alt="imgN2" className="w-full h-auto" />
-          <h1 className="text-center border-b-2 pb-2 border-cyan-950 dark:border-orange-50  text-cyan-950 dark:text-orange-50 font-bold text-4xl md:text-6xl">
+          <h1 className="pb-2 text-4xl font-bold text-center border-b-2 border-cyan-950 dark:border-orange-50 text-cyan-950 dark:text-orange-50 md:text-6xl">
             PASIÓN
           </h1>
 
-          <h1 className="text-center border-b-2 pb-2 border-cyan-950 dark:border-orange-50  text-cyan-950 dark:text-orange-50 font-bold text-3xl md:text-5xl">
+          <h1 className="pb-2 text-3xl font-bold text-center border-b-2 border-cyan-950 dark:border-orange-50 text-cyan-950 dark:text-orange-50 md:text-5xl">
             DISCIPLINA
           </h1>
         </div>
 
-        <div className=" flex flex-col justify-start gap-4">
+        <div className="flex flex-col justify-start gap-4 ">
           <div className="flex flex-col gap-4">
-            <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-3xl md:text-5xl">
+            <h1 className="text-3xl font-bold text-center text-cyan-950 dark:text-orange-50 md:text-5xl">
               WLP
             </h1>
-            <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-3xl">
+            <h1 className="text-3xl font-bold text-center text-cyan-950 dark:text-orange-50">
               Pasión, Disciplina y Compromiso
             </h1>
-            <p className="text-start text-black dark:text-white  md:text-xl">
+            <p className="text-black text-start dark:text-white md:text-xl">
               La lucha libre es un deporte emocionante, pero también desafiante.
               En WLP, sabemos que subir al ring implica riesgos, por eso te
               entrenamos con la mejor técnica, disciplina y seguridad. Cada
               llave, caída y movimiento requiere precisión, fuerza y control. No
               se trata solo de talento, sino de preparación.
             </p>
-            <p className="text-start text-black dark:text-white  md:text-xl">
+            <p className="text-black text-start dark:text-white md:text-xl">
               Para ser un verdadero luchador, debes comprometerte con tu cuerpo
               y mente: 
               <ul>
                 <li>
                   {" "}
-                  <span className="text-start font-bold text-cyan-950 dark:text-orange-50  md:text-xl">
+                  <span className="font-bold text-start text-cyan-950 dark:text-orange-50 md:text-xl">
                     Entrena con disciplina
                   </span>{" "}
                   para fortalecer tu resistencia y evitar lesiones.
                 </li>
                 <li>
                   {" "}
-                  <span className="text-start font-bold text-cyan-950 dark:text-orange-50  md:text-xl">
+                  <span className="font-bold text-start text-cyan-950 dark:text-orange-50 md:text-xl">
                     Aliméntate bien
                   </span>{" "}
                   para tener la energía y recuperación que tu cuerpo necesita.
                 </li>
                 <li>
                   {" "}
-                  <span className="text-start font-bold text-cyan-950 dark:text-orange-50  md:text-xl">
+                  <span className="font-bold text-start text-cyan-950 dark:text-orange-50 md:text-xl">
                     Sigue las instrucciones de tus entrenadores
                   </span>{" "}
                   y aprende a caer con seguridad.
                 </li>
               </ul>
             </p>
-            <p className="text-start text-black dark:text-white  md:text-xl">
+            <p className="text-black text-start dark:text-white md:text-xl">
               La lucha libre no es solo combate, es superación. Si entrenas con
               dedicación y respeto, dominarás el ring sin ponerte en peligro.
-              <span className="text-start font-bold text-cyan-950 dark:text-orange-50 md:text-xl">
+              <span className="font-bold text-start text-cyan-950 dark:text-orange-50 md:text-xl">
                 {" "}Lucha con honor, gana con pasión
               </span>
             </p>
-            <h1 className="text-center text-cyan-950 dark:text-orange-50 font-bold text-3xl ">
+            <h1 className="text-3xl font-bold text-center text-cyan-950 dark:text-orange-50 ">
               NO LO INTENTE EN CASA
             </h1>
           </div>
 
-          <button className="w-full dark:text-black flex justify-center p-2 transition-all duration-700 ease-out bg-sky-900 dark:bg-orange-50 md:text-2xl font-bold hover:bg-cyan-950 dark:hover:bg-orange-200">
+          <button className="flex justify-center w-full p-2 font-bold transition-all duration-700 ease-out dark:text-black bg-sky-900 dark:bg-orange-50 md:text-2xl hover:bg-cyan-950 dark:hover:bg-orange-200">
             Subir al Ring
           </button>
         </div>
