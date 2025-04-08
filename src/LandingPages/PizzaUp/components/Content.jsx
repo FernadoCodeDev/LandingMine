cec258988f46ab5c10a079031d63169825286003
Fernando <fernando.e.mejia.e@gmail.com>
2025-04-08 11:49:43 -0600

    Pizza Up Adjustment

--- src/LandingPages/PizzaUp/components/Content.jsx
+++ src/LandingPages/PizzaUp/components/Content.jsx
@@ -8,8 +8,8 @@ import Pizza6 from "../img/Pizza6.webp";
 
 const Content = () => {
   return (
-    <div className="grid items-center grid-cols-2 max-w-[80rem] m-auto gap-2 bg-gray-200 dark:bg-neutral-900 md:grid-cols-3">
-      <div className="flex flex-row justify-between col-span-2 p-2 text-black dark:text-white md:col-span-3">
+    <div className="p-2 grid items-center grid-cols-1 md:grid-cols-2 max-w-[80rem] m-auto gap-2 bg-gray-200 dark:bg-neutral-900 lg:grid-cols-3">
+      <div className="flex flex-col justify-between p-2 text-center text-black md:flex-row md:col-span-2 dark:text-white lg:col-span-3">
         <botton className="p-2 font-bold bg-gray-300 border-b-2 border-red-600 cursor-pointer dark:bg-neutral-800">
           Pizza
         </botton>
