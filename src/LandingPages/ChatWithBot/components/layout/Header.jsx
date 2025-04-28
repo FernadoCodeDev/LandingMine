4f60b018037014bc5fa280fabe46f3d98cb405fd
Fernando <fernando.e.mejia.e@gmail.com>
2025-04-28 13:25:28 -0600

    Image

--- src/LandingPages/ChatWithBot/components/layout/Header.jsx
+++ src/LandingPages/ChatWithBot/components/layout/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import ChatWithBotLogo from "../../assets/img/ChatWithBotLogo.webp";
 import DarkMode from "../../../GlobalComponents/DarkMode";
 
 const Header = () => {
   return (
-    <div className="relative w-full h-auto text-black bg-white dark:bg-neutral-800 dark:text-white ">
+    <div className="relative w-full h-auto text-black bg-white dark:bg-neutral-900 dark:text-white ">
       <div className="grid items-center h-auto grid-cols-2 gap-2 p-4 m-auto ">
-        <div className="m-auto">
-          <p className="">Charla con Bot </p>
-        </div>
+        <img
+          src={ChatWithBotLogo}
+          alt="ChatWithBotLogo"
+          className="w-full h-auto m-auto md:max-w-40"
+        />
 
         <div className="m-auto">
           <DarkMode />
--- src/LandingPages/ChatWithBot/components/section/FirstSection.jsx
+++ src/LandingPages/ChatWithBot/components/section/FirstSection.jsx
@@ -2,7 +2,18 @@ import React from "react";
 
 const FirstSection = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center w-full min-h-screen gap-4 p-4 text-black bg-white dark:text-white dark:bg-neutral-800"></div>
+    <div className="relative flex flex-col items-center justify-center w-full min-h-screen gap-4 p-4 text-black bg-white dark:text-white dark:bg-neutral-900">
+      <h1 className="text-2xl font-bold text-center md:text-4xl">
+        Hola ¿En qué puedo ayudarte?
+      </h1>
+
+      <div className="mx-auto max-w-[80rem] rounded-2xl flex flex-col w-full gap-2 p-2 mb-4 border bg-slate-100 dark:bg-neutral-800 border-emerald-800 focus:outline-none focus:ring focus:ring-emerald-800">
+        <input
+          className="w-full p-2 mr-2 bg-transparent flex-grow-1 border-2-emerald-800"
+          placeholder="Pregunta lo que quieras"
+        />
+      </div>
+    </div>
   );
 };
 
