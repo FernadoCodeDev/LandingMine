b3e01198a5a28a0cba4db3d55394da07e61ca7a4
Fernando <fernando.e.mejia.e@gmail.com>
2025-05-01 10:45:12 -0600

    Adjustment Height Camping Night

--- src/LandingPages/CampingNight/components/layout/HeaderCN.jsx
+++ src/LandingPages/CampingNight/components/layout/HeaderCN.jsx
@@ -13,7 +13,7 @@ const HeaderDH = () => {
         style={{ backgroundImage: `url(${Background})` }}
       ></div>
 
-      <div className="relative grid items-start h-auto grid-cols-2 gap-2 p-4 md:grid-cols-3">
+      <div className="relative grid items-start h-auto min-h-screen grid-cols-2 gap-2 p-4 md:grid-cols-3">
         <div className="order-3 col-span-2 mx-auto md:col-span-1 md:order-2 md:w-auto md:h-20 md:bg-white/20 md:bg-backdrop-blur-[2px]">
           <MobileMenu
             textColor="text-white transition-all duration-700 ease-out"
