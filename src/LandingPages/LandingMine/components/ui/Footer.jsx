bdeffcb08dee59484e334f313b6bd072e0525c99
Fernando <fernando.e.mejia.e@gmail.com>
2025-06-09 09:32:53 -0600

    Footer

--- src/LandingPages/LandingMine/components/ui/Footer.jsx
+++ src/LandingPages/LandingMine/components/ui/Footer.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { Trans, useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <footer className="w-full py-4 text-center text-white bg-blue-950">
-      <h3>
-        Puedes encontrar este y más trabajos mios en 
-        <a href="https://fercode.netlify.app/">
-          <span className="ml-1 font-bold text-white transition duration-500 ease-in-out hover:text-orange-300">
-             mí página personal
-          </span>
-        </a>
-      </h3>
+      <p>
+        <Trans
+          i18nKey="footer"
+          components={{
+            link: (
+              <a
+                href="https://fercode.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ml-1 font-bold text-white transition duration-500 ease-in-out hover:text-orange-300"
+              />
+            ),
+          }}
+        />
+      </p>
     </footer>
   );
 };
+
 export default Footer;
